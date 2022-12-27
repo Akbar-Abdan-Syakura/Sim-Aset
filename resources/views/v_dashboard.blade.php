10b2b7bba96dd012dc3b5b4d0e8c693846703d08
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2022-12-27 22:35:17 +0700

    menambahkan plugin di template lte, membuat views dashboard, membuat layouts main(utama)

--- resources/views/v_dashboard.blade.php
+++ resources/views/v_dashboard.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.v_main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-
-<body>
+@section('content')
     <h1>Selamat datang di halaman Dashboard</h1>
-</body>
-
-</html>
+    <h2>Sistem Informasi Manajemen Aset PT. Dapensi Trio Usaha</h2>
+    <h3><strong>Setelah selesai menggunakan aplikasi, jangan lupa untuk logout dari akun anda </strong></h3>
+@endsection
