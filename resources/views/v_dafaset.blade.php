29dd97782161e17c9ef48d62a60bac42862741d9
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-01-13 01:39:34 +0700

    mengubah tampilan content

--- resources/views/v_dafaset.blade.php
+++ resources/views/v_dafaset.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.v_main')
 
 @section('title')
-    Dashboard
+    Daftar Keseluruhan Aset
 @endsection
 @section('content')
     <h1>Daftar Barang Inventaris Aset Tetap PT. Dapensi Trio Usaha</h1>
