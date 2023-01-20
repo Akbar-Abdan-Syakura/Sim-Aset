40e11b1d01c80da1408aa584547101fda322fdbb
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-01-20 16:45:33 +0700

    mengubah tampilan dashboard

--- resources/views/partials/v_content.blade.php
+++ resources/views/partials/v_content.blade.php
@@ -3,7 +3,7 @@
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
+                <div class="col-sm-8">
                     <h1>
                         @yield('title')
                     </h1>
