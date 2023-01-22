d729e4699625b2ad2f739b244d8d2b2644ce1d29
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-01-22 20:17:23 +0700

    membuat table aset dan tabel relasinya

---
+++ app/Models/tb_kondisi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tb_kondisi extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_01_22_125757_create_cabangs_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCabangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cabangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kd_cabang');
+            $table->string('nama_cbng');
+            $table->text('alamat');
+            $table->enum('penempatan', [
+                'Kantor',
+                'Lorong Bawah',
+                'Mitra',
+                'Parkir Belakang',
+                'Parkir Depan',
+                'Ruang Atas Pantry',
+                'Ruang Dapur',
+                'Ruang Dirut',
+                'Ruang Gudang',
+                'Ruang IT',
+                'Ruang Keuangan',
+                'Ruang Loby',
+                'Ruang Lorong Atas',
+                'Ruang Luar',
+                'Ruang Mushola',
+                'Ruang Operasi dan Pemasaran',
+                'Ruang Pantry',
+                'Ruang Rapat',
+                'Ruang Remunerasi dan Layanan',
+                'Ruang SDM dan Sarana',
+                'Ruang Server',
+                'Ruang Setper dan Legal',
+                'Ruang Tamu',
+                'Ruang Umum dan SDM'
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cabangs');
+    }
+}
