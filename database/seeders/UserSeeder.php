e2b5dd108ea49af5de3d5f0978e2b4287d184c8c
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-02-05 06:07:50 +0700

    insert new seeding on aset and user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,28 +24,28 @@ class UserSeeder extends Seeder
                 "role_id" => 1
             ],
             [
-                "name" => "Indra",
+                "name" => "Indra Ibrahim",
                 "email" => "indra@gmail.com",
                 "email_verified_at" => now(),
                 "password" => Hash::make("admin"),
                 "role_id" => 1
             ],
             [
-                "name" => "Ilham",
-                "email" => "ilhma@gmail.com",
+                "name" => "Raden M Ilham",
+                "email" => "ilham@gmail.com",
                 "email_verified_at" => now(),
                 "password" => Hash::make("admin"),
                 "role_id" => 2
             ],
             [
-                "name" => "Akup",
-                "email" => "akup@gmail.com",
+                "name" => "Akub Zainal",
+                "email" => "akub@gmail.com",
                 "email_verified_at" => now(),
                 "password" => Hash::make("admin"),
                 "role_id" => 3
             ],
             [
-                "name" => "Andi",
+                "name" => "Andi Rochman",
                 "email" => "andi@gmail.com",
                 "email_verified_at" => now(),
                 "password" => Hash::make("admin"),
