a7d28c46bd9d489d1fe3ab727eea57919cfdfa28
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2023-02-07 07:14:26 +0700

    footer

--- resources/views/partials/v_footer.blade.php
+++ resources/views/partials/v_footer.blade.php
@@ -4,7 +4,7 @@
         Sistem Informasi Manajemen Aset PT. Dapensi Trio Usaha
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2021-2022 Akbar Abdansya.</strong> All rights
+    <strong>Copyright &copy; 2021-2023 Akbar Abdansya.</strong> All rights
     reserved.
 </footer>
 </div>
